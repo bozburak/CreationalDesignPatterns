29a205ab5fa4cf6d0d6375a5386cc515c7b6858a
Burak BOZ <boz-burak-trash@hotmail.com>
2020-05-16 22:56:17 +0300

    Create solution, create projects,write singleton pattern

---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton singleton1 = Singleton.Instance;
+            Singleton singleton2 = Singleton.Instance;
+            if (object.ReferenceEquals(singleton1, singleton2))
+            {
+                Console.WriteLine("Work");
+            }
+            else
+            {
+                Console.WriteLine("Broke");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SingletonPattern/Singleton.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq.Expressions;
+
+namespace SingletonPattern
+{
+    public class Singleton
+    {
+        private static Singleton _instance = null;
+
+        private Singleton() => Expression.Empty();
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Singleton();
+                }
+                return _instance;
+            }
+        }
+    }
+}
