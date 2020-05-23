3d3ccc7f364cf80996bd715dcfe514ba787a9d49
Burak BOZ <boz-burak-trash@hotmail.com>
2020-05-23 12:50:31 +0300

    Prototype Pattern

---
+++ PrototypePattern/Models/ColorManager.cs
@@ -0,0 +1,15 @@
+﻿using PrototypePattern.Abstracts;
+using System.Collections.Generic;
+
+namespace PrototypePattern.Models
+{
+    class ColorManager
+    {
+        private Dictionary<string, ColorPrototype> _colors = new Dictionary<string, ColorPrototype>();
+        public ColorPrototype this[string key]
+        {
+            get { return _colors[key]; }
+            set { _colors.Add(key, value); }
+        }
+    }
+}
--- PrototypePattern/Program.cs
+++ PrototypePattern/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using PrototypePattern.Models;
+using System;
 
 namespace PrototypePattern
 {
@@ -10,6 +7,13 @@ namespace PrototypePattern
     {
         static void Main(string[] args)
         {
-        }
+            ColorManager colorManager = new ColorManager();
+            colorManager["red"] = new Color(255, 0, 0);
+            colorManager["green"] = new Color(0, 255, 0);
+            colorManager["blue"] = new Color(0, 0, 255);
+
+            Color color1 = colorManager["red"].Clone() as Color;
+            Color color2 = colorManager["green"].Clone() as Color;
+            Color color3 = colorManager["blue"].Clone() as Color;            Console.ReadKey();        }
     }
 }
