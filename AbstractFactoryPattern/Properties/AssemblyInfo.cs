e273a21b5ec7752dfde1f5c62f450232d0df044b
Burak BOZ <boz-burak@hotmail.com>
2021-01-25 18:09:03 +0300

    Update AssemblyInfo.cs

--- AbstractFactoryPattern/Properties/AssemblyInfo.cs
+++ AbstractFactoryPattern/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("AbstractFactoryPattern")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Turkcell Iletisim Hizmetleri A.S.")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AbstractFactoryPattern")]
-[assembly: AssemblyCopyright("Copyright © Turkcell Iletisim Hizmetleri A.S. 2020")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
